fba19376d3c104dcdd83e6ccc97c28d064cb1141
Anzgal <agalvan70@hotmail.com>
2021-12-10 14:31:07 -0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'smCard': '320px',
+        'mdCard': '328px',
+        'lgCard': '368px',
+      },
+      gap: {
+        'customGap': '16px',
+      }
+    },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '759px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1140px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 }
